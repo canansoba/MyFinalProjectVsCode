ebef8ccce649598bd4221f31ea6437dc0a0dae83
Canan <Canan@ASUS>
2021-03-12 20:39:59 +0300

    12.03.2021

---
+++ src/app/models/product.ts
@@ -0,0 +1,11 @@
+export interface Product{
+    id:number;
+    brandId:number;
+    colorId:number;
+    // brandName:string;
+    // colorName:string;
+    modelYear:number;
+    dailyPrice:number;
+    description:string;
+}
+
---
+++ src/app/models/rental.ts
@@ -0,0 +1,7 @@
+export interface Rental{
+    rentalId:number;
+    productId:number;
+    customerId:number;
+    rentalDate:Date;
+    dailyPrice:Date;
+}
